2b34e05b214216ea9401cedd129b3903a58cf4fc
Joel Byler <joelbyler@gmail.com>
2014-04-13 14:10:26 -0400

    Update production to not minify resources

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,6 @@ BootstrapApp::Application.configure do
   config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.digest = true  
+  config.assets.digest = true 
+  config.assets.js_compressor = Uglifier.new(mangle: false) 
 end
