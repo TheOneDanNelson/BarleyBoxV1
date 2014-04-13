21f1e2b5faced396a675af467e7b538e4aad72f6
Joel Byler <joelbyler@gmail.com>
2014-04-12 23:22:36 -0400

    Make the sliders work

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,6 +1,31 @@
 
-var BarleyBox = angular.module('BarleyBox',[]);
+var BarleyBox = angular.module('BarleyBox', ['BarleyBoxFilters', 'ui.slider']);
 
 function BeerSelector($scope) {
-    $scope.beers = brews;
+  $scope.beers = brews;
+  $scope.abv_range=[0, 100];
+  $scope.ibu_range=[0, 100];
+  $scope.color_range=[0, 100];
 }
+
+angular.module('BarleyBoxFilters', [])
+.filter('abv', function(){
+  return function(beers, abv_range){
+    if (!abv_range)return beers;
+    return _.reject(beers, function(beer) { return !(beer.apv >= abv_range[0] && beer.apv <= abv_range[1]) });
+  }
+}).filter('ibu', function(){
+  return function(beers, ibu_range){
+    if (!ibu_range)return beers;
+    return _.reject(beers, function(beer) { return !(beer.ibu >= ibu_range[0] && beer.ibu <= ibu_range[1]) });
+  }
+}).filter('colors', function(){
+  return function(beers, color_range){
+    if (!color_range)return beers;
+    return _.reject(beers, function(beer) { return !(beer.srm >= color_range[0] && beer.srm <= color_range[1]) });
+  }
+});
+
+
+
+
